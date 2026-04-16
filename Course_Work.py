5776e838a462580cac7ff1a06dc486a8883022af
yosh <rytulis7@gmail.com>
2026-04-16 17:09:20 +0300

    Update Course_Work.py

--- Course_Work.py
+++ Course_Work.py
@@ -1,32 +1,61 @@
-class Sort:
-    def __init__(self):
-        self._list = []
-        self._sorted_list = []
+import random
 
-    def list(self, lst):
-        self._list = lst
+class Sort:
+    def __init__(self, lst):
+        if lst:
+            self._list = lst
+        else:
+            raise ValueError("Please provide a non empty list")
+        
+        self._sorted_list = []
+        self._counter = 0
 
     @property
     def isSorted(self):
         return(all(self._list[i] <= self._list[i+1] for i in range(len(self._list)-1)))
     
+    @property
     def sort_list(self):
         i=0
         while not self.isSorted:
             if self._list[i] > self._list[i+1]:
                 self._list[i], self._list[i+1] = self._list[i+1], self._list[i]
-                i+=1
-            else:
+                self._counter+=1
+            i+=1
+            if i==len(self._list)-1:
                 i=0
         return self._list
 
-    def sorted_list(self):
-        self._sorted_list = self.sort_list()
-        print(self._sorted_list)
+    @property
+    def print_sorted_list(self):
+        self._sorted_list = self.sort_list
+        print(f"Sorted list: {self._sorted_list}")
+        print(f"times swapped: {self._counter}")
+
+    @property
+    def get_sorted_list(self):
+        self._sorted_list = self.sort_list
+        return(self._sorted_list)
     
-lst = [4, 3, 1, 6, 7]
-sorteD = Sort()
+class BubbleSort(Sort):
+    pass
 
-sorteD.list(lst)
+class MergeSort(Sort):
+    pass
 
-sorteD.sorted_list()
+class QuickSort(Sort):
+    pass
+
+class BogoSort(Sort):
+    def __init__(self, lst):
+        super().__init__(lst)
+    
+    @property
+    def sort_list(self):
+        while not self.isSorted:
+            num1=random.randrange(0, len(self._list))
+            num2=random.randrange(0, len(self._list))
+            if num1 is not num2:
+                self._list[num1], self._list[num2] = self._list[num2], self._list[num1]
+                self._counter+=1
+        return self._list
