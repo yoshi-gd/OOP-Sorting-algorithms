2dda133452dfdbb6a8c111cb0f8d5ee61b591ac0
yosh <rytulis7@gmail.com>
2026-04-22 19:37:44 +0300

    Update Course_Work.py

--- Course_Work.py
+++ Course_Work.py
@@ -3,7 +3,7 @@ import random
 class Sort:
     def __init__(self, lst):
         if lst:
-            self._list = lst
+            self._list = lst.copy()
         else:
             raise ValueError("Please provide a non empty list")
         
@@ -15,24 +15,25 @@ class Sort:
         return(all(self._list[i] <= self._list[i+1] for i in range(len(self._list)-1)))
     
     @property
-    def sort_list(self):
+    def _sort_list(self):
+        self._counter = 0
         return sorted(self._list)
 
     @property
     def print_sorted_list(self):
-        print(f"Sorted list: {self.sort_list}")
+        print(f"Sorted list: {self._sort_list}")
         print(f"Comparisons: {self._counter}")
 
     @property
     def get_sorted_list(self):
-        return(self.sort_list)
+        return(self._sort_list)
     
 class BubbleSort(Sort):
     def __init__(self, lst):
         super().__init__(lst)
 
     @property
-    def sort_list(self):
+    def _sort_list(self):
         while not self.isSorted:
             for i in range(0, len(self._list)-1):
                 self._counter+=1
@@ -45,7 +46,7 @@ class MergeSort(Sort):
         super().__init__(lst)
 
     @property
-    def sort_list(self):
+    def _sort_list(self):
         left = 0
         right = len(self._list)-1
         self.split(left, right)
@@ -95,14 +96,43 @@ class MergeSort(Sort):
             k+=1
 
 class QuickSort(Sort):
-    pass
+    def __init__(self, lst):
+        super().__init__(lst)
+
+    @property
+    def _sort_list(self):
+        low = 0
+        high = len(self._list) - 1
+        self.quicksort(low, high)
+        return self._list
+
+    def quicksort(self, low, high):
+        if low<high:
+            pivot = self.part_sort(low, high)
+
+            self.quicksort(low, pivot-1)
+            self.quicksort(pivot+1, high)
+
+    def part_sort(self, low, high):
+        pivot = self._list[high]
+
+        i = low-1
+
+        for j in range(low, high):
+            self._counter+=1
+            if self._list[j] < pivot:
+                i+=1
+                self._list[i], self._list[j] = self._list[j], self._list[i]
+
+        self._list[i+1], self._list[high] = self._list[high], self._list[i+1]
+        return (i+1)
 
 class BogoSort(Sort):
     def __init__(self, lst):
         super().__init__(lst)
     
     @property
-    def sort_list(self):
+    def _sort_list(self):
         while not self.isSorted:
             num1=random.randrange(0, len(self._list))
             num2=random.randrange(0, len(self._list))
@@ -117,7 +147,7 @@ class HeapSort(Sort):
         super().__init__(lst)
 
     @property
-    def sort_list(self):
+    def _sort_list(self):
         length = len(self._list)
 
         for i in range(length//2, -1, -1):
@@ -137,11 +167,81 @@ class HeapSort(Sort):
 
         if left < length and self._list[left] > self._list[largest]:
             largest = left
+            self._counter+=1
 
         if right < length and self._list[right] > self._list[largest]:
             largest = right
+            self._counter+=1
 
         if largest != i:
             self._list[i], self._list[largest] = self._list[largest], self._list[i]
             self.max_heap(length, largest)
-        
+
+print("Welcome! What kind of list are you sorting? (Enter corresponding number)")
+print("1. Numbers")
+print("2. Words")
+s1 = int(input("Option: "))
+if s1 != 1 and s1 !=2:
+    raise Exception("Option does not exist, please try again")
+print()
+
+print("What type of sorting algorithm would you like to use? (Enter corresponding number)")
+print("1. Bubble Sort")
+print("2. Merge Sort")
+print("3. Quick Sort")
+print("4. Bogo Sort")
+print("5. Heap Sort")
+print("6. Python sorting")
+s2 = int(input("Option: "))
+if s2 not in range(1,7):
+    raise Exception("Option does not exist, please try again")
+print()
+
+print("Do you want a new file with the unsorted and sorted list")
+print("Or the sorted list added to current file")
+print("Or rewrite the file with the sorted list?")
+print("1. New")
+print("2. Add")
+print("3. Rewrite")
+s3 = int(input("Option: "))
+if s3 not in range(1, 4):
+    raise Exception("Option does not exist, please try again")
+print()
+
+print("Enter file name/file path")
+f_name = input("Name: ")
+
+if s3 == 1:
+    print("New file name")
+    f_new = input("Name:")
+
+if s1 == 1:
+    with open(f_name, 'r') as file:
+        List=[int(number) for number in file.read().split()]
+else:
+    with open(f_name, 'r') as file:
+        List=[word.strip() for word in file.read().split(',')]
+
+algorithms = {
+    1: BubbleSort(List),
+    2: MergeSort(List),
+    3: QuickSort(List),
+    4: BogoSort(List),
+    5: HeapSort(List),
+    6: Sort(List)
+}
+
+S=algorithms[s2]
+
+if s3 == 1:
+    f = open(f_new, "x")
+    with open(f_new, "w") as file:
+        file.write(f"Unsorted list: {List}\n")
+        file.write(f"Sorted list: {S.get_sorted_list}\n")
+elif s3 == 2:
+    with open(f_name, "a") as file:
+        file.write("\n")
+        file.write(f"Sorted list: {S.get_sorted_list}")
+else:
+    with open(f_name, "w") as file:
+        file.write(f"Sorted list: {S.get_sorted_list}")
